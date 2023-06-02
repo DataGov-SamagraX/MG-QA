cc47f372aa9a6fb832f23c313884a8098b48104f
psn0w <08yaksh08@gmail.com>
2023-06-02 14:45:31 +0530

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,70 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useRef } from "react";
+import { jsonData } from "./data";
+import './App.css'
 
-function App() {
+const App = () => {
+  const [searchQuery, setSearchQuery] = useState("");
+  const [selectedQuestion, setSelectedQuestion] = useState(null);
+  const audioRef = useRef();
+  const filteredData = jsonData.filter((data) =>
+    data.Que.toLowerCase().includes(searchQuery.toLowerCase())
+  );
+
+  const handleQuestionClick = (question) => {
+    setSelectedQuestion(question);
+    if (audioRef.current) {
+      audioRef.current.pause();
+      audioRef.current.load();
+      audioRef.current.play();
+    }
+  };
+
+  const handleSearchChange = (e) => {
+    setSearchQuery(e.target.value);
+    setSelectedQuestion(null);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app-container">
+      <h1 className="app-heading">Talk to Mahatma Gandhi</h1>
+      <input
+        type="text"
+        placeholder="Search question..."
+        value={searchQuery}
+        onChange={handleSearchChange}
+        className="search-input"
+      />
+
+      <ul className="question-list">
+        {filteredData.map((data, index) => (
+          <li
+            key={index}
+            onClick={() => handleQuestionClick(data)}
+            className={
+              selectedQuestion === data
+                ? "question-item selected"
+                : "question-item"
+            }
+          >
+            {data.Que}
+          </li>
+        ))}
+      </ul>
+
+      {selectedQuestion && (
+        <div className="answer-container">
+          <h3 className="question">{selectedQuestion.Que}</h3>
+          <p className="answer">{selectedQuestion.ans}</p>
+          {selectedQuestion.audio_file && (
+            <audio controls className="audio-player" ref={audioRef}>
+              <source src={selectedQuestion.audio_file} type="audio/mpeg" />
+              Your browser does not support the audio element.
+            </audio>
+          )}
+        </div>
+      )}
     </div>
   );
-}
+};
 
 export default App;
---
+++ src/data.js
@@ -0,0 +1,14 @@
+export const jsonData = [
+  { Que: "Who are you", ans: "I am MG", audio_file: "./data/english.mp3" },
+  {
+    Que: "What is your name",
+    ans: "My name is MG",
+    audio_file: "./data/plain.mp3",
+  },
+  {
+    Que: "Where are you from",
+    ans: "I am from Earth",
+    audio_file:
+      "./data/X2Download.app - Mahatma Gandhi's Speech _ Gandhi On God and Truth _ Eclectic (192 kbps).mp3",
+  },
+];
